511a1302c5f0bc200df445859d90b360b801e686
a8365885 <z8365885@gmail.com>
2019-04-03 15:52:54 +0800

    init

---
+++ QRCodeScanner/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.RoyalBlue
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(640, 480)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 499)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 100)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Scan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(120, 500)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(533, 99)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(665, 611)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "QRCode Scanner"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ QRCodeScanner/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports Emgu.CV
+Imports ZXing
+
+Public Class Form1
+
+    Dim cap As New VideoCapture
+    Dim barcodeReader As New BarcodeReader
+    Dim nameList As New Dictionary(Of String, String)
+    Dim priceList As New Dictionary(Of String, String)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim streamReader As IO.StreamReader = My.Computer.FileSystem.OpenTextFileReader(".\\products.csv")
+        Dim line As String
+        Do
+            line = streamReader.ReadLine()
+            If line Is Nothing Then Exit Do
+
+            Dim splitLine As String() = line.Split(",")
+            Dim id As String = splitLine(0)
+            Dim name As String = splitLine(1)
+            Dim price As String = splitLine(2)
+            nameList.Add(id, name)
+            priceList.Add(id, price)
+        Loop
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            PictureBox1.Image = cap.QueryFrame.Bitmap()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Dim result As Result = barcodeReader.Decode(PictureBox1.Image)
+        If result IsNot Nothing Then
+            Timer1.Enabled = False
+            Dim decoded As String = result.ToString().Trim()
+            If nameList.ContainsKey(decoded) Then
+                Try
+                    TextBox1.Text = "商品: " + nameList.Item(decoded) + vbCrLf + "價格: $" + priceList.Item(decoded)
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+            Else
+                TextBox1.Text = "查無此商品"
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = ""
+        Timer1.Enabled = True
+    End Sub
+End Class
