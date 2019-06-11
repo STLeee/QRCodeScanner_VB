c389029c1d64eedd20f53565b810e2474ba2e787
STLeee <z8365885@gmail.com>
2019-06-11 12:21:08 +0800

    update

--- QRCodeScanner/Form1.vb
+++ QRCodeScanner/Form1.vb
@@ -49,6 +49,7 @@ Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         TextBox1.Text = ""
+        Timer1.Interval = 100
         Timer1.Enabled = True
     End Sub
 End Class
